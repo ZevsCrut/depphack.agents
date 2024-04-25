c587015af9b96e441a90a52be7b1bdf152e9fe0e
ZevsCrut <matvey.zimin.02@mail.ru>
2024-04-25 16:45:56 +0300

    make some changes

--- handlers.py
+++ handlers.py
@@ -1,4 +1,4 @@
-from loader import bot, TOKEN
+from loader import bot, TOKEN,dp,router
 
 import os
 from aiogram import types
@@ -13,10 +13,13 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
 
-from loader import dp, start_markup,router
+import loader
 
-
-
+class States(StatesGroup):
+    Start = State()
+    Profile_1 = State()
+    Profile_2 = State()
+    Profile_3 = State()
 
 
 @router.message(CommandStart())
@@ -27,13 +30,38 @@ async def cmd_start(message: types.Message, state: FSMContext) -> None:
         pass
     logo = types.FSInputFile('start_logo.png')
     await bot.send_photo(chat_id=message.chat.id, photo=logo,
-                         caption=f'Привет,{hbold(message.from_user.full_name)}!\n'
-                                 f'Я - твой личный помощник для выполнения любых научных задач',
-                         reply_markup=start_markup,
+                         caption=loader.text1,
+                         reply_markup=loader.start_markup,
                          )
 
+    #работа со стартовыми кнопками
+@router.callback_query(lambda c: c.data == 'about')
+async def about_bot(callback: types.callback_query) -> None:
+    await callback.message.answer(text=loader.about_project)
+    await callback.answer()
 
-#ВСЕГДА НИЖЕ ВСЕХ !!!
+@router.callback_query(lambda c: c.data == 'assistant_help')
+async def ass_help(callback: types.callback_query) -> None:
+    await callback.message.answer(text='тут должно быть чтото написано, но наш копирайтер чут чут даун ((((')
+    await callback.answer()
+@router.callback_query(lambda c: c.data == 'choose')
+async def choose_profile(callback: types.callback_query) -> None:
+    await callback.message.answer(text="Выберите профиль ассистента",
+                                  reply_markup = loader.choose_markup)
+    await callback.answer()
+
+    #переключение на профиль
+
+@router.callback_query(FSMContext(None),lambda c: c.data == 'profile_1')
+async def profile_1_start(callback: types.callback_query,state:FSMContext):
+    await state.set_state(States.Profile_1)
+    await callback.message.answer('вы выбрали первый профиль! напишите хуйню - мы ответим')
+
+@router.message(FSMContext(States.Profile_1),F.text)
+async def profile_1_speaking(message: types.Message):
+
+
+# ВСЕГДА НИЖЕ ВСЕХ !!!
 @router.message()
 async def echo_handler(message: types.Message) -> None:
     try:
